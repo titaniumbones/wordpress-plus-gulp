2fb06ce4ab1762e2c7d3f14a45dba2a658348df3
Matt Price <matt.price@utoronto.ca>
2017-11-12 17:06:30 -0500

    actually make this work

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM monostream/nodejs-gulp-bower
 COPY ./run-gulp.sh /
 RUN chmod a+x /run-gulp.sh \
-    && mkdir /active-project
+    && mkdir -p /wordpress/wp-content/themes/
 
-WORKDIR /themes
+WORKDIR /wordpress/wp-content/themes
 CMD ["/run-gulp.sh"]
--- run-gulp.sh
+++ run-gulp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-if [[ !  "$(ls -A /themes/understrap)" ]]; then
-  git clone https://github.com/titaniumbones/understrap.git -b dockerize /themes/understrap
-fi
-cd /themes/understrap
+mkdir -p /wordpress/wp-content/themes/understrap
+# if [[ !  "$(ls -A /wordpress/wp-content/themes/understrap)" ]]; then
+  git clone https://github.com/titaniumbones/understrap.git \
+      -b dockerize /wordpress/wp-content/themes/understrap
+# fi
+cd /wordpress/wp-content/themes/understrap
 npm install gulp
 npm install -d
 while true; do
